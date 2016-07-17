c35af1f255faefc5faee997551b62cac4f298c51
hanshenrik <hh.gronsleth@gmail.com>
2016-07-17 18:51:34 +0200

    Add CV section, smooth scrolling and much more

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,19 @@
+var scrollSpeed = 500;
+
 $(document).ready(function(){
   // Set copyright year to current year
-  $( '#current-year' ).text( (new Date).getFullYear() )
+  $( '#current-year' ).text( (new Date).getFullYear() );
+
+  // Scroll smoothly to element with id matching clicked element
+  $('.scroll-link').click(function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    $('html, body').animate({ scrollTop: $( id ).offset().top - 20 }, scrollSpeed);
+  });
+
+  // $('.highlight-link').click(function(e) {
+  //   e.preventDefault();
+  //   var id = $(this).attr('href');
+  //   $( id ).css('background', 'yellow');
+  // });
 })
