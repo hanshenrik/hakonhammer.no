b9b8e90fe02d69111306cfa2aed81b7aba4fb83e
hanshenrik <hh.gronsleth@gmail.com>
2016-07-19 23:49:10 +0200

    Add info page, style it and update links

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 var scrollSpeed = 500;
+var backgrounds = [0, 1, 2, 3, 4, 5];
+var colors = ["#14061b", "#032928", "#fdfdfd", "#c6ddff", "#e1d9e7"];
+
+var currentBackgroundIndex = 0;
+var currentColorIndex = 0;
 
 $(document).ready(function(){
   // Set copyright year to current year
@@ -16,4 +21,13 @@ $(document).ready(function(){
   //   var id = $(this).attr('href');
   //   $( id ).css('background', 'yellow');
   // });
+
+  $('#no-background').click(function() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    $('body').css('background', colors[currentColorIndex])
+  })
+  $('#toggle-background').click(function() {
+    currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
+    $('body').css('background', 'url(../images/background-'+currentBackgroundIndex+'.jpg)')
+  })
 })
