5fdccbfca4c47a2ded948456e6d6ef7f0dc8d6a0
hanshenrik <hh.gronsleth@gmail.com>
2016-11-27 18:46:57 +0100

    Shmitloads

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
   // Set copyright year to current year
   $( '#current-year' ).text( (new Date).getFullYear() );
 
-  $('.category-content').click(function(e) {
-    $(this).find('.category-text').slideToggle();
+  $('.category-item').hover(function(e) {
+    $(this).find('.category-text').stop().slideToggle();
     $(this).find('.read-more-icon').toggleClass('fa-rotate-180');
+    $(this).parent().toggleClass('open');
   });
 
 })
