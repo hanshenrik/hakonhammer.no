61e328fc23d6482b33bac1b595ed3a4c4d6c4587
hanshenrik <hh.gronsleth@gmail.com>
2017-01-07 21:30:06 +0100

    Footer, standardize transparent black colormobile layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 
-$(document).ready(function(){
+$( document ).ready( function() {
   // Set copyright year to current year
   $( '#current-year' ).text( (new Date).getFullYear() );
 
-  $('.category-item').click(function(e) {
-    $(this).find('.category-content').toggleClass('open');
-    $(this).find('.category-text').stop().slideToggle();
-    $(this).find('.read-more-icon').toggleClass('fa-rotate-180');
+  $( '.category-item' ).click( function( e ) {
+    $( this ).find( '.category-content' ).toggleClass( 'open' );
+    $( this ).find( '.category-text' ).stop().slideToggle();
+    $( this ).find( '.read-more-icon' ).toggleClass( 'fa-rotate-180' );
   });
 })
