4bf82980cdd753a18e14b93e4fd4ff084ea9cc4f
hanshenrik <hh.gronsleth@gmail.com>
2017-01-07 20:46:52 +0100

    Shaitloads

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,9 @@ $(document).ready(function(){
   // Set copyright year to current year
   $( '#current-year' ).text( (new Date).getFullYear() );
 
-  $('.category-item').hover(function(e) {
+  $('.category-item').click(function(e) {
+    $(this).find('.category-content').toggleClass('open');
     $(this).find('.category-text').stop().slideToggle();
     $(this).find('.read-more-icon').toggleClass('fa-rotate-180');
-    $(this).parent().toggleClass('open');
   });
-
 })
